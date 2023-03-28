0ed7cbc93752c8add910d97ca4fc424fb7397555
eunseo <esjeong153@naver.com>
2023-03-28 14:11:32 +0900

    Add files via upload

--- test.py
+++ test.py
@@ -59,76 +59,67 @@ meteor = evaluate.load('meteor')
 performance = {}
 result = {}
 
-for epoch in range(30):
-    performance[epoch] = {}
-    result[epoch] = {}
+model.eval()
+with torch.no_grad():
+    # 모델 불러올 때 일치하지 않는 키들을 무시 
+    # model.load_state_dict(torch.load("/home/nlplab/hdd1/eunseo/AutoVQA/results/0309__real_dl_cls_latent_768/checkpoints/0_mymodel.pt", map_location=device), strict=False)
+    # model.to(device)
+    model.load_state_dict(torch.load(model_path, map_location=device))
+    model.to(device)    
 
-    model.eval()
-    with torch.no_grad():
-        # 모델 불러올 때 일치하지 않는 키들을 무시 
-        # model.load_state_dict(torch.load("/home/nlplab/hdd1/eunseo/AutoVQA/results/0309__real_dl_cls_latent_768/checkpoints/0_mymodel.pt", map_location=device), strict=False)
-        # model.to(device)
-        model.load_state_dict(torch.load(model_path, map_location=device))
-        model.to(device)    
-
-        eval_bleu = []
-        eval_rouge1 = []
-        eval_rougeL = []
-        eval_meteor = [] # machine translation metric
+    eval_bleu = []
+    eval_rouge1 = []
+    eval_rougeL = []
+    eval_meteor = [] # machine translation metric
 
 
-        for i, batch in enumerate(tqdm(eval_dataloader)):
-            result[epoch][i] = {}
+    for i, batch in enumerate(tqdm(eval_dataloader)):
+        result[i] = {}
 
-            mode = "eval"
-            img_path = batch[0]
-            images = batch[1]
-            image_ids = batch[2]
-            ids = batch[3]
-            captions = batch[4]
+        mode = "eval"
+        img_path = batch[0]
+        images = batch[1]
+        image_ids = batch[2]
+        ids = batch[3]
+        captions = batch[4]
 
-            # captions = [f"<|endoftext|>{caption}<|endoftext|>" for caption in captions]
+        # captions = [f"<|endoftext|>{caption}<|endoftext|>" for caption in captions]
+    
+        logits = model.generate(device, images)
+        # print(logits.shape) # [64, 3, 50261]
+        label = list(captions)
+        # label = model.tokenizer.batch_decode([:,:], skip_special_tokens=True)
+        pred = model.tokenizer.batch_decode(logits, skip_special_tokens=False)
+        pred = list(map(lambda x:x.split("<|endoftext|>")[1], pred))
         
-            logits = model.generate(device, images)
-            # print(logits.shape) # [64, 3, 50261]
-            label = list(captions)
-            # label = model.tokenizer.batch_decode([:,:], skip_special_tokens=True)
-            pred = model.tokenizer.batch_decode(logits, skip_special_tokens=False)
-            pred = list(map(lambda x:x.split("<|endoftext|>")[1], pred))
-            
-            # print(captions)
-            # print(pred)
+        # print(captions)
+        # print(pred)
 
-            eval_bleu.append(bleu.compute(predictions=pred, references=label)["bleu"])
-            eval_rouge1.append(rouge.compute(predictions=pred, references=label)["rouge1"])
-            eval_rougeL.append(rouge.compute(predictions=pred, references=label)["rougeL"])
-            eval_meteor.append(meteor.compute(predictions=pred, references=label)['meteor'])
-            # print(eval_bleu)
-            # print(eval_rouge1)
-            # print(eval_rougeL)
-            # print(eval_meteor)
-            
-            for img_path, label, pred in zip(img_path, label, pred):
-                result[epoch][i]['img_path'] = img_path
-                result[epoch][i]['label'] = label
-                result[epoch][i]['pred'] = pred
+        eval_bleu.append(bleu.compute(predictions=pred, references=label)["bleu"])
+        eval_rouge1.append(rouge.compute(predictions=pred, references=label)["rouge1"])
+        eval_rougeL.append(rouge.compute(predictions=pred, references=label)["rougeL"])
+        eval_meteor.append(meteor.compute(predictions=pred, references=label)['meteor'])
+        
+        for img_path, label, pred in zip(img_path, label, pred):
+            result[i]['img_path'] = img_path
+            result[i]['label'] = label
+            result[i]['pred'] = pred
 
-                
-        print("eval BLEU in epoch {}: {}".format(str(epoch), str(sum(eval_bleu)/len(eval_bleu))))
-        print("eval Rouge1 in epoch {}: {}".format(str(epoch), str(sum(eval_rouge1)/len(eval_rouge1))))
-        print("eval RougeL in epoch {}: {}".format(str(epoch), str(sum(eval_rougeL)/len(eval_rougeL))))
-        print("eval METEOR in epoch {}: {}".format(str(epoch), str(sum(eval_meteor)/len(eval_meteor))))
+    # print("eval BLEU in epoch {}: {}".format(str(epoch), str(sum(eval_bleu)/len(eval_bleu))))
+    # print("eval Rouge1 in epoch {}: {}".format(str(epoch), str(sum(eval_rouge1)/len(eval_rouge1))))
+    # print("eval RougeL in epoch {}: {}".format(str(epoch), str(sum(eval_rougeL)/len(eval_rougeL))))
+    # print("eval METEOR in epoch {}: {}".format(str(epoch), str(sum(eval_meteor)/len(eval_meteor))))
+    
+    performance['eval_bleu'] = str(sum(eval_bleu)/len(eval_bleu))
+    performance['eval_rouge1'] = str(sum(eval_rouge1)/len(eval_rouge1))
+    performance['eval_rougeL'] = str(sum(eval_rougeL)/len(eval_rougeL))
+    performance['eval_meteor'] = str(sum(eval_meteor)/len(eval_meteor))
+    
+    
+    with open(test_performance_save, "w") as file:
+        json.dump(performance, file, indent=4)
+    
+    with open(test_result_save, "w") as file:
+        json.dump(result, file, indent=4)
         
-        performance[epoch]['eval_bleu'] = str(sum(eval_bleu)/len(eval_bleu))
-        performance[epoch]['eval_rouge1'] = str(sum(eval_rouge1)/len(eval_rouge1))
-        performance[epoch]['eval_rougeL'] = str(sum(eval_rougeL)/len(eval_rougeL))
-        performance[epoch]['eval_meteor'] = str(sum(eval_meteor)/len(eval_meteor))
-        
-        
-        with open(test_performance_save, "w") as file:
-            json.dump(performance, file, indent=4)
-        
-        with open(test_result_save, "w") as file:
-            json.dump(result, file, indent=4)
-            
     
